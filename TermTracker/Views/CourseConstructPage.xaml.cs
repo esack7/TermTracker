e0f5fb6ae00a3099dd2656aac34734e4eb34b99f
Isaac Heist <isaacheist@hotmail.com>
2021-02-05 20:05:03 -0800

    Started building CourseConstructPage with minimal functionality.

---
+++ TermTracker/Views/CourseConstructPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TermTracker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CourseConstructPage : ContentPage
+    {
+        public CourseConstructPage()
+        {
+            InitializeComponent();
+            SaveEditButton.IsVisible = false;
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void SaveEditButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
